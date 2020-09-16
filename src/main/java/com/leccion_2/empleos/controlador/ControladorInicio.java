71afdb46441f34240ea47157a1306f6524f6702e
chainick-byte <igorrepyakh@gmail.com>
2020-09-16 18:13:24 +0100

    tocando la segunda vez lo que es el thymeleaf y ver con que se lo comen y para que sirve y etc

--- src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
+++ src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
@@ -5,10 +5,46 @@
  */
 package com.leccion_2.empleos.controlador;
 
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author igorr
  */
+@Controller
 public class ControladorInicio {
     
+    @GetMapping("/")
+    public String mostrarIndex(Model model ){
+        String nombre="auxiliar de contabilidad";
+        Date fechaPub=new Date();
+        double salario=9000.0;
+        boolean vigente =true;
+        
+        //al pasar al metodo el objeto de tipo model, 
+        //podemos agregar a este modelo cualquier tipo de dato
+        model.addAttribute("mensaje", "hola mundo mundial");
+        //funciona de siguiente modo addAttribute primer valor es el nombre de la variable
+        // el valor de la variable 
+        //OJO:variable puede ser string int lista u otro objecto que se necesita pasar 
+        model.addAttribute("fecha", new Date());
+        //de esta forma se demuestra que podemos pasar cualquier objet !!!
+        
+        
+        
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("fechaPub", fechaPub);
+        model.addAttribute("salario", salario);
+        model.addAttribute("vigente", vigente);
+        return "index";
+    }
+    
 }
+ 
+
+
+
+
