23bb93c836b868493bc9266061afd2d1665104dc
chainick-byte <igorrepyakh@gmail.com>
2020-09-18 01:13:17 +0100

    añado tag th:each de thymeleaf y autogenero una lista en la vista

--- src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
+++ src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
@@ -6,6 +6,8 @@
 package com.leccion_2.empleos.controlador;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ControladorInicio {
+    
+    @GetMapping("listado")
+    public String mostrarLista(Model model){
+        List<String> lista= new LinkedList<String>();
+        lista.add("Ingeniero de sistemas");
+        lista.add("Auxiliar de contabilidad");
+        lista.add("Vendedor");
+        lista.add("Arquitecto");
+        model.addAttribute("empleos", lista);
+        return "listado";
+    }
     
     @GetMapping("/")
     public String mostrarIndex(Model model ){
