5ca536565fb22f8ff4afaf3549aae32d4eaacfc4
chainick-byte <igorrepyakh@gmail.com>
2020-09-18 17:31:56 +0100

    añado lista de varios objetos 'vacante' en una tabla autogenerada

--- src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
+++ src/main/java/com/leccion_2/empleos/controlador/ControladorInicio.java
@@ -6,6 +6,8 @@
 package com.leccion_2.empleos.controlador;
 
 import com.leccion_2.empleos.modelos.Vacante;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +21,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ControladorInicio {
+    @GetMapping("/tabla")
+    public String mostrarTabla(Model model) throws ParseException{
+        List<Vacante> lista=getVacantes();
+        model.addAttribute("vacantes", lista);
+        
+        return "tabla";
+    }
     
     @GetMapping("/detalle")
     public String mostrarDetalle(Model model){
@@ -29,6 +38,41 @@ public class ControladorInicio {
         vacante.setSalario(9700.0);  
         model.addAttribute("vacante", vacante);
         return "detalle";
+    }
+    private List<Vacante> getVacantes() throws ParseException{
+        SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
+        List<Vacante> listaVacantes= new LinkedList<Vacante>();
+       try{ 
+        //primera oferta de trabajo 
+        Vacante vacante1 =new Vacante();
+        vacante1.setId(1);
+        vacante1.setNombre("Arquitecto");
+        vacante1.setDescripcion("Se necesita un arquitecto de software");
+        vacante1.setFecha(sdf.parse("08-02-2020"));
+        vacante1.setSalario(15000.0);
+        //creo segunda oferta de trabajo 
+        Vacante vacante2 =new Vacante();
+        vacante2.setId(2);
+        vacante2.setNombre("Desarralador web");
+        vacante2.setDescripcion("Se busca diseñador web / maquetador con 4 años de experiencia");
+        vacante2.setFecha(sdf.parse("04-10-2019"));
+        vacante2.setSalario(12700.0);  
+        //tercera oferta detrabajo 
+        Vacante vacante3 =new Vacante();
+        vacante3.setId(3);
+        vacante3.setNombre("becario");
+        vacante3.setDescripcion("buscamos un esclavo particular para bdsm party");
+        vacante3.setFecha(sdf.parse("01-11-1847"));
+        vacante3.setSalario(1000.0);
+        
+        listaVacantes.add(vacante1);
+        listaVacantes.add(vacante2);
+        listaVacantes.add(vacante3);
+        
+       }catch(ParseException e){
+           System.out.println("Error: " +e.getMessage());
+       }
+       return listaVacantes;
     }
     
     @GetMapping("/listado")
