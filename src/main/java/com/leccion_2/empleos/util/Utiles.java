043e76b47435a512b3e873a020c1e6b142d99600
chainick-byte <igorrepyakh@gmail.com>
2020-10-01 15:50:48 +0100

    añado la funcionalidad de siguiente tipo :1)eliminar espacios,2)añadir cadena de texto aleatorio al nombre de imagen 3)definir variables a nivel de la aplicacion en aplication.properties y por medio de la anotacion @Value introducirlas en los puntos donde se necesitan

--- src/main/java/com/leccion_2/empleos/util/Utiles.java
+++ src/main/java/com/leccion_2/empleos/util/Utiles.java
@@ -15,18 +15,33 @@ public class Utiles {
         //obtenemos el nombre original de archivo
         String nombreOriginal = multiPart.getOriginalFilename();
         nombreOriginal=nombreOriginal.replace(" ", "_");
+        String nombreFinal=randomAlphaNumeric(28)+nombreOriginal;
         try {
             //Aqui se forma el nombre de archivo que queremos guardar
-            File imagenFile = new File(ruta + nombreOriginal);
+            File imagenFile = new File(ruta + nombreFinal);
             System.out.println("Archivo" + imagenFile.getAbsolutePath());
             //guardar fiscamente el archivo
             multiPart.transferTo(imagenFile);
-            return nombreOriginal;
+            return nombreFinal;
         } catch (IOException e) {
             System.out.println("Error" + e.getMessage());
             return null;
         }
 
+    }
+    /**
+     * metodo para generar una cadena aleatoria de longitud N
+     * @param count
+     * @return
+     */
+    public static String randomAlphaNumeric(int count){
+        String CARACTERES="zaqwsxcderfvbgtyhnmjuikñpç1234567890";
+        StringBuilder sBuilder=new StringBuilder();
+        while(count--!=0){
+            int caracter=(int)(Math.random()*CARACTERES.length());
+            sBuilder.append(CARACTERES.charAt(caracter));
+        }
+        return sBuilder.toString();
     }
 
 }
